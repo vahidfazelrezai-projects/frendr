67114c05ce5a8e58ab4c37fe9d766e09bdd1ce3d
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2016-07-22 19:06:56 -0700

    add in firebase

--- respond.js
+++ respond.js
@@ -1,3 +1,14 @@
-module.exports = function (message, senderId) {
-  return message;
+var firebase = require('firebase');
+firebase.initializeApp({
+  serviceAccount: "firebase-heroku-credentials.json",
+  databaseURL: "https://frendr-de57d.firebaseio.com/"
+});
+var db = firebase.database();
+var ref = db.ref("/messages");
+
+var respond = function (message, senderId) {
+  ref.push(message);
+  return 'message recorded';
 }
+
+module.exports = respond;
